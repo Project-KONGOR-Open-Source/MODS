b1e96a0d3d5c3ee8d8dbd7453a10532655d69aef
[K]ONGOR <project.kongor@proton.me>
2025-02-16 20:13:04 +0000

    Update To Version 1.4.1.0

--- manager/HON Mod Manager/Properties/AssemblyInfo.cs
+++ manager/HON Mod Manager/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
 
 [assembly: AssemblyTitle("HoN Mod Manager")]
 [assembly: AssemblyDescription("Heroes of Newerth Modification Manager")]
@@ -10,6 +10,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("26db8f1e-0f44-4810-829e-11d5c3edcbfd")]
-[assembly: AssemblyVersion("1.4.0.1")]
-[assembly: AssemblyFileVersion("1.4.0.1")]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("ModMan CS Test")]
+[assembly: AssemblyVersion("1.4.1.0")]
+[assembly: AssemblyFileVersion("1.4.1.0")]
+[assembly: InternalsVisibleTo("ModMan CS Test")]
